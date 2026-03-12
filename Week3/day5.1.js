3d615b53caeb482d694d7c173e6ae08759939b9b
ayushd69 <ayushdarne45@gmail.com>
2026-03-12 16:39:44 +0530

    new

---
+++ Week3/day5.1.js
@@ -0,0 +1,18 @@
+//frontend depend on the backend but backend doesnot depend on front end..
+//http,API,databases this three are main to develop bACkend.
+//client always make the requestt
+//request was handle by backend
+//for each http request there is each http response.
+//http request 1.post=to create,2.get=to read,3.put=to update,4.delete=to delete.  11 types are there but this 4 are imp.
+//client should know address of http server to send request
+//address=http://domain.port/url-path .eg this is sufficient to make request by client
+//API should preoccess the request 
+//http recieve the request and API will handle the request..
+//there no free compiler for js so there is js runtime using chrome. before 2010
+//now  we have nodejs. it is an js compiler.
+//v8 most efficient and popular engine of js nodejs also v8 engine...
+//node js conatian world largest library.. almost 2.5 lKH pacakges.
+//reequest npm and download the package and use it.
+//npm install module-name for downloading.
+//express in one of the module of nodejs among 2.5 lak module it is very imp.
+ 
